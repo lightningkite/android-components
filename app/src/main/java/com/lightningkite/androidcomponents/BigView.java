72bd23e7d524ccd6d6fa43fc154900f8a89f1ae1
jivie <joseph@lightningkite.com>
2015-06-02 09:39:00 -0600

    Initial commit

---
+++ app/src/main/java/com/lightningkite/androidcomponents/BigView.java
@@ -0,0 +1,12 @@
+package com.lightningkite.androidcomponents;
+
+import android.view.View;
+
+/**
+ * Created by jivie on 6/2/15.
+ */
+public class BigView extends View {
+    public BigView(BigViewContainer container){
+
+    }
+}
---
+++ app/src/main/java/com/lightningkite/androidcomponents/BigViewContainer.java
@@ -0,0 +1,8 @@
+package com.lightningkite.androidcomponents;
+
+/**
+ * Created by jivie on 6/2/15.
+ */
+public interface BigViewContainer {
+    Context getContext();
+}
