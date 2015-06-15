c319fcc48532df93a02c9ef40a7c1d8b97770fc7
shanelk <shane@meosphere.com>
2015-06-15 15:17:00 -0600

    Made field EXTRA_BIGVIEW_NAME protected

--- src/main/java/com/lightningkite/androidcomponents/bigview/BasicBVCActivity.java
+++ src/main/java/com/lightningkite/androidcomponents/bigview/BasicBVCActivity.java
@@ -13,7 +13,7 @@ import android.os.Bundle;
  */
 public abstract class BasicBVCActivity extends Activity implements BigViewContainer {
 
-    private static final String EXTRA_BIGVIEW_NAME = "BasicBVCActivity.name";
+    protected static final String EXTRA_BIGVIEW_NAME = "BasicBVCActivity.name";
     public BigView mView;
 
     /**
